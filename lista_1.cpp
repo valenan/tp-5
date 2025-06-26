a2eefd0486b4556f35bc8ec333f622bb6389d7d4
huser-12 <valen.ang86@gmail.com>
2025-06-26 18:51:02 -0300

    punto 1

--- lista_1.cpp
+++ lista_1.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 using namespace std;
 typedef struct tnodo *punt;
-typedef struct tnodo{
+struct tnodo{
     int dato;
     punt sig;
 };
+void menu(int &n);
 void crear_lista(punt &lista);
 bool es_vacia(punt lista);
 void nuevo_nodo(punt &nuevo);
@@ -13,11 +14,108 @@ void agregar_ordenado(punt &lista, punt nuevo);
 void agregar_final(punt &lista,punt nuevo);
 bool buscar(punt lista, int dato);
 punt eliminar_inicio(punt &lista);
+punt eliminar_final(punt &lista);
 punt eliminar_nodo(punt &lista, int dato);
 void mostrar_lista(punt lista);
 int minimo(punt lista);
 int main(){
 punt lista,nuevo;
+int opcion, dato,min;
+cout<<"Bienvenido al programa de manejo de listas enlazadas simples"<<endl;
+cout<<"Para empezar, cree una lista vacía."<<endl;
+crear_lista(lista);
+do{
+    menu(opcion);
+switch(opcion)
+{
+case 1:
+    
+    crear_lista(lista);
+    cout<<"Lista creada."<<endl;
+    break;
+case 2:
+    nuevo_nodo(nuevo);
+    agregar_inicio(lista, nuevo);
+break;
+case 3:
+    nuevo_nodo(nuevo);
+    agregar_ordenado(lista, nuevo);
+    break;
+case 4:
+    nuevo_nodo(nuevo);
+    agregar_final(lista, nuevo);
+    break;  
+case 5:
+    cout<<"Ingrese el dato a buscar: "; 
+    cin>>dato;
+    if(buscar(lista, dato)){
+        cout<<"El dato "<<dato<<" se encuentra en la lista."<<endl;
+    }
+    else{
+        cout<<"El dato "<<dato<<" no se encuentra en la lista."<<endl;
+    }
+
+    break;
+case 6:
+    nuevo = eliminar_inicio(lista); 
+    if(nuevo != NULL){
+        cout<<"Nodo eliminado al inicio con dato: "<<nuevo->dato<<endl;
+        delete nuevo; // Liberar memoria
+    }
+    else{
+        cout<<"No se pudo eliminar, la lista está vacía."<<endl;
+    }
+    break;
+case 7:
+    nuevo = eliminar_final(lista);
+    if(nuevo != NULL){
+        cout<<"Nodo eliminado al final con dato: "<<nuevo->dato<<endl;
+        delete nuevo; // Liberar memoria
+    }
+    else{
+        cout<<"No se pudo eliminar, la lista está vacía."<<endl;
+    }
+    break;
+case 8:
+    cout<<"Ingrese el dato a eliminar: ";
+    cin>>dato;
+    nuevo = eliminar_nodo(lista, dato);
+    if(nuevo != NULL){
+        cout<<"Nodo eliminado con dato: "<<nuevo->dato<<endl;
+        delete nuevo; // Liberar memoria
+    }
+    else{
+        cout<<"No se pudo eliminar, el nodo no se encuentra en la lista."<<endl;
+    }
+    break;
+case 9:
+    cout<<"Mostrando lista: ";
+
+    mostrar_lista(lista);
+    break;
+case 10:
+    min = minimo(lista);
+    if (min != -999) {
+        cout << "El valor mínimo en la lista es: " << min << endl;
+    }
+    break;
+case 11:
+    if(es_vacia(lista)){
+        cout<<"La lista está vacía."<<endl;
+    }
+    else{
+        cout<<"La lista no está vacía."<<endl;
+    }
+    break;
+case 0:
+    cout<<"Saliendo del programa."<<endl;
+    break;
+default:
+    cout<<"Opción no válida, por favor intente de nuevo."<<endl;
+    break;
+}
+}while(opcion!=0);
+
 
     return 0;
 };
@@ -168,4 +266,20 @@ else{
     };
 }
 return min;
+};
+void menu (int &n){
+    cout << "Menu de opciones:" << endl;
+    cout << "1. Crear lista" << endl;
+    cout << "2. Agregar nodo al inicio" << endl;
+    cout << "3. Agregar nodo ordenado" << endl;
+    cout << "4. Agregar nodo al final" << endl;
+    cout << "5. Buscar nodo" << endl;
+    cout << "6. Eliminar nodo al inicio" << endl;
+    cout << "7. Eliminar nodo al final" << endl;
+    cout << "8. Eliminar nodo específico" << endl;
+    cout << "9. Mostrar lista" << endl;
+    cout << "10. Encontrar mínimo" << endl;
+    cout<<"11. Lista vacia?"<<endl;
+    cout << "0. Salir" << endl;
+    cin >> n;
 };
